f8e811e501bdf7294f927432731f49d8ee5325fd
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-03-31 20:54:02 +0900

    changed from tcpip_adapter_init to esp_netif_init

--- main/main.c
+++ main/main.c
@@ -84,8 +84,16 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 #if CONFIG_AP_MODE
 void wifi_init_softap()
 {
+#if ESP_IDF_VERSION_MAJOR >= 4 && ESP_IDF_VERSION_MINOR >= 1
+	ESP_LOGI(TAG,"ESP-IDF esp_netif");
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_ap();
+#else
+	ESP_LOGI(TAG,"ESP-IDF tcpip_adapter");
 	tcpip_adapter_init();
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
+#endif
 
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
