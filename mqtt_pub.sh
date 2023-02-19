df135cfbe1a31846b3ffdb7370319b607a5ae21e
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-02-19 12:39:59 +0900

    Update mqtt_pub.sh

--- mqtt_pub.sh
+++ mqtt_pub.sh
@@ -4,12 +4,10 @@
 #
 
 #set -x
-ip="192.168.10.141"
 mdns="esp32-broker.local"
 while :
 do
 	payload=`date "+%Y/%m/%d-%H:%M:%S"`
-	#mosquitto_pub -h ${ip} -p 1883 -t "topic" -m ${payload}
 	mosquitto_pub -h ${mdns} -p 1883 -t "topic" -m ${payload}
 	sleep 1
 done
